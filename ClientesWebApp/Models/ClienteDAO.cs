305cf07e2fe697a7bfcb898929443c8e77cf88ab
Felipe <f.maldonadom@alumnos.duoc.cl>
2017-10-22 09:54:40 -0300

    carga de proyecto cliente .net

---
+++ ClientesWebApp/Models/ClienteDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using ClientesWebApp.Models;
+
+namespace ClientesWebApp.Models
+{
+    public class ClienteDAO
+    {
+        //creamos el cliente para acceder al servicio web
+        // y crear peticiones tanto get, post, delete o put
+
+        //y se agrega la url del servicio
+        RestClient client = new RestClient("http://localhost:55681/api/");
+
+        public List<Cliente> ListadoClientes()
+        {
+            //creamos una peticion de tipo get que apunte a http://localhost:55681/api/clientes
+            RestRequest request = new RestRequest("clientes", Method.GET);
+
+            //obtnemos un list de clientes a partir del request anterior 
+            IRestResponse<List<Cliente>> response = client.Execute<List<Cliente>>(request);
+
+            return response.Data;
+        }
+
+
+        public bool AgregarCliente(Cliente cliente)
+        {
+            RestRequest request = new RestRequest("clientes", Method.POST);
+
+            IRestResponse response = client.Execute(request);
+
+            // si el error es 500 significa qie ocurrio un error al crear el cliente
+            if(response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return false;
+            }
+
+            if(response.StatusCode  == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
